17974aeb968c11ef595d02e776fc9a6a12859716
whitney <whitney.njogu@student.moringaschool.com>
2022-08-22 01:15:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName=`bob`;
+
+function upperCaseCustomerName() {
+    customerName=customerName.toUpperCase();
+}
+
+var bestCustomer=`not bob`
+function setBestCustomer(){
+    bestCustomer=`not bob`;
+}
+setBestCustomer();
+
+function overwriteBestCustomer() {
+    return bestCustomer= `maybe bob`;
+}
+
+function changeLeastFavoriteCustomer() {
+    const leastFavoriteCustomer= `fifi`;
+    leastFavoriteCustomer=`kamami`;
+}
